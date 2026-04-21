ff853ef25fbd546184c79146fa8a43c55ae91790
elon1te9 <elon1te9@gmail.com>
2026-04-21 20:09:47 +0300

    autorization

--- GuessWord.Client/Models/AuthUserInfo.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace GuessWord.Client.Models
-{
-    public class AuthUserInfo
-    {
-        public string UserId { get; set; } = string.Empty;
-        public string Login { get; set; } = string.Empty;
-    }
-}
