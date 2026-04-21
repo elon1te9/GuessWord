a0d59f7e192fcc2128c278f0be55e78bef990edd
elon1te9 <elon1te9@gmail.com>
2026-04-21 17:12:11 +0300

    new_bug

--- GuessWord.Client/Program.cs
+++ GuessWord.Client/Program.cs
@@ -2,9 +2,9 @@ using GuessWord.Client;
 using GuessWord.Client.Services;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using MudBlazor.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
@@ -12,8 +12,6 @@ builder.Services.AddScoped(sp => new HttpClient
 {
     BaseAddress = new Uri("https://localhost:7172/")
 });
-
-//builder.Services.AddMudServices();
 
 builder.Services.AddScoped<ApiRequestService>();
 builder.Services.AddScoped<AuthService>();
